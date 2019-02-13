55a4bbe0aa96f8d2883794ef7a9da2e675434ba9
maor bargig <echo_logic@maors-MacBook-Air.local>
2019-02-13 17:44:53 +0200

    fixes

--- js/main.js
+++ js/main.js
@@ -2,39 +2,61 @@
 
 var gCanvas;
 var gCtx;
-var gProperties = {
-    Line: document.querySelector('#color-line').value,
-    Fill: document.querySelector('#color-fill').value,
-    shapes: document.querySelector('#shapes').value
-};
+var gProperties = { shape: 'circle', line: 'black', fill: 'white'};
+
+
 
 function init() {
     gCanvas = document.querySelector('#mister-canvas');
     gCtx = gCanvas.getContext('2d')
 }
+function getShape(el) {
+    gProperties.shape = el;
+}
 
 
+
+
+function getColor(el, key) {
+    if (key === 'line') {
+        gProperties.line = el;
+        console.log(el)
+    } else {
+        gProperties.fill = el;
+    }
+    console.log(gProperties)
+}
+
+
+function getLineColor() {
+    var elLine = document.querySelector('#color-line');
+    var color = elLine.value;
+    console.log(color)
+}
 function canvasClicked(ev) {
-    console.log(gProperties.shapes);
-    
-    switch (gProperties.shapes) {
-        
-        case square:
-        drawCircle(ev.offsetX, ev.offsetY);
-        break;
-        
-        case  circle:
-        drawSquare(ev.offsetX, ev.offsetY);
-        break;
-        
-        case triangle:
-        drawTriangle(ev.offsetX, ev.offsetY);
+    console.log(gProperties.shape);
+
+    switch (gProperties.shape) {
+
+        case "square":
+            drawSquare(ev.offsetX, ev.offsetY);
+
             break;
-    
-        default :
-        drawLine(ev.offsetX, ev.offsetY);
+
+        case "circle":
+            drawCircle(ev.offsetX, ev.offsetY);
+
+
             break;
-        }
+
+        case "triangle":
+            drawTriangle(ev.offsetX, ev.offsetY);
+            break;
+
+        default:
+            drawLine(ev.offsetX, ev.offsetY);
+            break;
+    }
 
 }
 
@@ -46,28 +68,31 @@ function drawLine(X, Y) {
 
 
 function drawTriangle(X, Y) {
+    console.log(gProperties.fill)
     gCtx.save()
     gCtx.beginPath();
     gCtx.moveTo(X, Y - 20);
     gCtx.lineTo(X + 20, Y + 20);
     gCtx.lineTo(X - 20, Y + 20);
-    gCtx.fillStyle = `${gColor.Fill}`
-    gCtx.strokeStyle = `${gColor.Line}`
+    gCtx.fillStyle = `${gProperties.fill}`
+    gCtx.strokeStyle = `${gProperties.line}`
     gCtx.lineWidth = 10
     gCtx.closePath()
     gCtx.stroke();
     gCtx.fill()
     gCtx.restore()
 }
-function drawSquare() {
+function drawSquare(X, Y) {
     gCtx.save()
     gCtx.beginPath();
-    gCtx.moveTo(X- 20, Y - 20);
+    gCtx.moveTo(X - 20, Y - 20);
     gCtx.lineTo(X + 20, Y - 20);
     gCtx.lineTo(X + 20, Y + 20);
     gCtx.lineTo(X - 20, Y + 20);
-    gCtx.fillStyle = `${gColor.Fill}`
-    gCtx.strokeStyle = `${gColor.Line}`
+
+    gCtx.fillStyle = `${gProperties.fill}`
+    gCtx.strokeStyle = `${gProperties.line}`
+
     gCtx.lineWidth = 10
     gCtx.closePath()
     gCtx.stroke();
@@ -75,13 +100,18 @@ function drawSquare() {
     gCtx.restore()
 }
 
-function drawCircle() {
+function drawCircle(X,Y) {
     gCtx.save()
     gCtx.beginPath()
-    gCtx.arc(100, 300, 50, 0, 2 * Math.PI);
-    gCtx.fillStyle = `${gColor.Fill}`
-    gCtx.strokeStyle = `${gColor.Line}`
+    // gCtx.moveTo(X, Y);
+
+    gCtx.arc(X, Y, 50, 0, 2 * Math.PI);
+
+    gCtx.fillStyle = `${gProperties.fill}`
+    gCtx.strokeStyle = `${gProperties.line}`
     gCtx.closePath()
+    gCtx.lineWidth = 10
+
     gCtx.stroke()
     gCtx.fill()
     gCtx.restore()
